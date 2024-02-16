97fff6444328382053924f1e3750b9fe26a545f8
Yoshimax <116089729+Yoshimax69@users.noreply.github.com>
2024-02-16 23:54:44 +0100

    cambios

---
+++ src/lib/inputs/components/text.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { TextFormFieldSchema } from './../processors/text.processor';
+    export let data: TextFormFieldSchema;
+</script>
+
+
+<input type="text">
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script lang="ts">
+    import Text from './../lib/inputs/components/text.svelte';
+</script>
+	
+
 <h1>Reto de disashop</h1>
 <p></p>
+
+<Text></Text>

b40dc4fdcc01532b53ebcb822f24fe49ae964483
Alabasted <ikercapi08@gmail.com>
2024-02-16 22:56:42 +0100

    components

---
+++ src/lib/inputs/checkbox.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let field_readonly: boolean;
+    export let field_description: string;
+    export let field_default_value: boolean;
+    
+
+</script>
+
+{#if field_readonly}
+<input class="patata" type="Checkbox" readonly >
+{:else}
+<input class="patata" type="Checkbox" >
+{/if}
+
+<label for="patata">{field_description}</label>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+	import Checkbox from './../lib/inputs/checkbox.svelte';
+
+
+
+</script>
+
 <h1>Reto de disashop</h1>
 <p></p>
+
+<Checkbox>
+    
+</Checkbox>
+

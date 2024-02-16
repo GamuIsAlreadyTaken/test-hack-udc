00970d72825f7e9dd9a8acde826914d117965940
Alabasted <ikercapi08@gmail.com>
2024-02-17 00:14:26 +0100

    Checkbox arreglado

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,6 @@
 	import type { CheckboxFormFieldSchema } from './../lib/inputs/processors/checkbox.processor';
 	import Checkbox from './../lib/inputs/components/checkbox.svelte';
 
-	let data:CheckboxFormFieldSchema = {
-	field_default_value: true,
-	field_dependent_on: {
-		field_id: 1
-	}
-
-	};
 	let field_readonly = true;
 	let field_description = "CHECK";
 
@@ -17,4 +10,4 @@
 <h1>Reto de disashop</h1>
 <p></p>
 
-<Checkbox data={field_description} {field_readonly}></Checkbox>
+<input type="checkbox" disabled> 

72530e0a99f24e036fbfb2dfd394f40f1ae6d0a4
Alabasted <ikercapi08@gmail.com>
2024-02-17 00:34:01 +0100

    Numbers

---
+++ src/lib/inputs/components/number.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { CheckboxFormFieldSchema } from './../processors/checkbox.processor';
+    export let data: CheckboxFormFieldSchema;
+
+</script>
+
+{#if data.field_readonly}
+<input class="numero" type="number" readonly>
+{:else}
+<input class="numero" type="number">
+{/if}
+
+<label>{data.field_description}</label>     

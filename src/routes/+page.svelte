6e5c70bc360a0c58a98b4df5ec863c5c467cc6f8
Alabasted <ikercapi08@gmail.com>
2024-02-16 23:38:22 +0100

    Checkbox creada,no terminada

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-	import Checkbox from './../lib/inputs/checkbox.svelte';
+	import Checkbox from './../lib/inputs/components/checkbox.svelte';
 
-
+	let field_default_value = true;
+	let field_readonly = true;
+	let field_description = "CHECK";
 
 </script>
 
 <h1>Reto de disashop</h1>
 <p></p>
 
-<Checkbox>
-    
-</Checkbox>
+<Checkbox {field_default_value} {field_description}  {field_readonly}></Checkbox>
 

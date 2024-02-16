88032279cdece0f236e89e196c3c93950ba6d323
Yoshimax <116089729+Yoshimax69@users.noreply.github.com>
2024-02-16 21:53:07 +0100

    components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+    import Checkbox from '$lib/inputs/checkbox.svelte';
+    import Text from './../lib/inputs/text.svelte';
+</script>
 <h1>Reto de disashop</h1>
 <p></p>
+
+<Checkbox></Checkbox>

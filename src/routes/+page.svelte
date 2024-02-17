6a62ed07d35a7e349858889c3c356be3e5678b0b
Yoshimax <116089729+Yoshimax69@users.noreply.github.com>
2024-02-17 02:26:35 +0100

    iker tus muertos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,4 @@
 <script lang="ts">
-<<<<<<< HEAD
-    import type { TextFormFieldSchema } from '$lib/inputs/processors/text.processor';
-	import Text from './../lib/inputs/components/text.svelte';
-	
-	let data: TextFormFieldSchema ={
-		field_description: "patata",
-
-		field_id: 3,
-
-		field_type: "text",
-
-		field_order: 4,
-
-		field_name: "",
-
-		field_readonly:false,
-		
-		field_default_value:"hola",
-
-
-		field_validations:{
-			min_length:30,
-
-			max_length:1
-		}
-
-		
-	}
-
-
-=======
->>>>>>> ffc2bcd810ed9babb495960aec322446d730b53d
 </script>
 
 <h1>Reto de disashop</h1>

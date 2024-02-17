0e55d179ac49aa38cfb9feb4a2b08dc95b3b35d7
Yoshimax <116089729+Yoshimax69@users.noreply.github.com>
2024-02-17 08:06:26 +0100

    intento de decorado de textbox

--- src/lib/inputs/text.svelte
+++ src/lib/inputs/text.svelte
@@ -27,8 +27,8 @@
 
 <label for={data.field_name}>
     {data.field_description}<br />
-
-    <input
+    <div class="prueba">
+        <input
         class="textbox"
         name={data.field_id}
         type="text"
@@ -37,7 +37,56 @@
         {maxlength}
         {pattern}
         readonly={data.field_readonly}
-    />
+        />
+    </div>
 </label>
+
+<style>
+
+@keyframes gradient { 
+  0%{background-position:0 0}
+  100%{background-position:100% 0}
+}
+    .prueba {
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  max-width: 400px;
+  margin: 0 auto;
+  border-radius: 2px;
+  padding: 1.4rem 2rem 1.6rem;
+  background: rgba(57, 63, 84, 0.8);
+  &:after {
+    content: "";
+    position: absolute;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+    z-index: 999;
+    height: 2px;
+    border-bottom-left-radius: 2px;
+    border-bottom-right-radius: 2px;
+    background-position: 0% 0%;
+    background: linear-gradient(to right, #B294FF, #57E6E6, #FEFFB8, #57E6E6, #B294FF, #57E6E6);
+    background-size: 500% auto;
+    animation: gradient 3s linear infinite;
+  }
+}
+
+.textbox {
+  flex-grow: 1;
+  color: BFD2FF;
+  font-size: 1.8rem;
+  line-height: 2.4rem;
+  vertical-align: middle;
+  &::-webkit-input-placeholder {
+    color: #7881A1;
+  }
+}
+
+
+</style>
+
 
 

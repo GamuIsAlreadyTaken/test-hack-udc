577a9cdabacba857dab082318d13e61edce791dc
Yoshimax <116089729+Yoshimax69@users.noreply.github.com>
2024-02-17 06:36:57 +0100

    Icono de carga

--- src/routes/jose/+page.svelte
+++ src/routes/jose/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import Chargeicon from './../../lib/assets/chargeicon.svelte';
     import type { PageData } from "./$types";
 
     export let data: PageData;
 </script>
+
+<Chargeicon></Chargeicon>

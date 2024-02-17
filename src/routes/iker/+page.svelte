47716a168e7bec319a857238c189deb3016d9bfd
Alabasted <ikercapi08@gmail.com>
2024-02-17 03:49:06 +0100

    TMTUPQYLQY

--- src/routes/iker/+page.svelte
+++ src/routes/iker/+page.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
-	import Text from '$lib/inputs/components/text.svelte';
-import type { TextFormFieldSchema } from '$lib/inputs/components/text.svelte';
+    import type { NumberFormFieldSchema } from '$lib/inputs/components/number.svelte';
+    import Number from '$lib/inputs/components/number.svelte';
   
-    let data: TextFormFieldSchema = {
-        field_description: "",
-        field_id: 3,
-        field_type: "text",
+    let data: NumberFormFieldSchema = {
+        field_description: "Descripcion:",
+        field_id: "3",
+        field_type: "number",
         field_order: 4,
         field_name: "",
         field_readonly: false,
-        field_default_value: "hol",
+        field_default_value: 2,
         field_validations:{
-            max_length:5,
-            min_length:3,
-            format:"[a-zA-Z0-9]+"
+            max_value: 4,
+
+            min_value: 1           
         }
     };
 
 
 </script>
 
-<Text {data}></Text>
+<Number {data}></Number>

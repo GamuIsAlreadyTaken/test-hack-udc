ffc2bcd810ed9babb495960aec322446d730b53d
Gamu <GamuDiez@gmail.com>
2024-02-17 02:15:17 +0100

    Segregacion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,57 +1,8 @@
 <script lang="ts">
-	import type{ SelectFormFieldSchema } from './../lib/inputs/processors/selection.processor.ts';
-    import type { TextFormFieldSchema } from '$lib/inputs/processors/text.processor';
-	import Text from './../lib/inputs/components/text.svelte';
-	import type { CheckboxFormFieldSchema } from './../lib/inputs/processors/checkbox.processor';
-    import Selection from '$lib/inputs/components/selection.svelte';
-	
-	let data: TextFormFieldSchema ={
-		field_description: "patata",
-
-		field_id: 3,
-
-		field_type: "text",
-
-		field_order: 4,
-
-		field_name: "",
-
-		field_readonly:false,
-		
-		field_default_value:"hola"
-
-
-	}
-
-	let data_sel: SelectFormFieldSchema={
-		field_description: "patata",
-
-		field_id: 3,
-
-		field_type: "select",
-
-		field_order: 4,
-
-		field_name: "",
-
-		field_readonly:false,
-		
-		field_default_value:"hola",
-
-		field_validations:{
-			options:[
-				"Africa",
-				"ASia",
-				"Europa",
-			]
-		}
-	}
-
-
 </script>
 
 <h1>Reto de disashop</h1>
 
-<!-- <Text {data}></Text> -->
-<Selection data={data_sel}></Selection>
-
+<a href="/iker">Tests de Iker</a>
+<a href="/jose">Tests de Jose</a>
+<a href="/victor">Tests de Victor</a>

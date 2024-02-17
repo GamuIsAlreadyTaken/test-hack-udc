0b3b277753fe9be3e4335ceb25b366951e18374b
Gamu <GamuDiez@gmail.com>
2024-02-17 02:01:24 +0100

    form builder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,7 @@
 <script lang="ts">
-	import Date from './../lib/inputs/components/date.svelte';
-    import type { DateFormFieldSchema } from '$lib/inputs/processors/date.processor';
-	
-	let dataP: DateFormFieldSchema ={
-		field_description: "",
-
-		field_id: 3,
-
-		field_type: "date",
-
-		field_order: 4,
-
-		field_name: "date",
-
-		field_readonly:true,
-		
-		field_default_value:null
-	}
-	</script>
+	import FormBuilder from "$lib/form-builder.svelte";
+</script>
 
 <h1>Reto de disashop</h1>
 
-<Date data = {dataP}></Date>
+<FormBuilder></FormBuilder>

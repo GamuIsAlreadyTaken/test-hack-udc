4c62d67d5565d752eb5df700d8d0947f0b8f60df
Yoshimax <116089729+Yoshimax69@users.noreply.github.com>
2024-02-17 09:04:02 +0100

    hola

--- src/lib/inputs/text.svelte
+++ src/lib/inputs/text.svelte
@@ -5,7 +5,6 @@
         MaxLength,
         MinLength,
     } from "$lib/types/form-validations";
-    import { writable, type Writable } from "svelte/store";
 
     export type TextFormFieldSchema = GenericFormFieldSchema & {
         field_type: "text";
@@ -20,7 +19,6 @@
 
 <script lang="ts">
     export let data: TextFormFieldSchema;
-    export let value: Writable<string> = writable(data.field_default_value);
 
     let minlength = (data.field_validations as MinLength).min_length;
     let maxlength = (data.field_validations as MaxLength).max_length;
@@ -28,72 +26,21 @@
 </script>
 
 <label for={data.field_name}>
-    {data.field_description}
-    <div>
+    {data.field_description}<br />
+    <div class="prueba">
         <input
-            name={data.field_id}
-            type="text"
-            bind:value={$value}
-            {minlength}
-            {maxlength}
-            {pattern}
-            readonly={data.field_readonly}
+        class="textbox"
+        name={data.field_id}
+        type="text"
+        bind:value={data.field_default_value}
+        {minlength}
+        {maxlength}
+        {pattern}
+        readonly={data.field_readonly}
         />
     </div>
 </label>
 
-<style>
-    @keyframes gradient {
-        0% {
-            background-position: 0 0;
-        }
-        100% {
-            background-position: 100% 0;
-        }
-    }
-    div {
-        position: relative;
-        display: flex;
-        flex-direction: row;
-        width: 100%;
-        max-width: 400px;
-        margin: 0 auto;
-        border-radius: 2px;
-        padding: 1.4rem 2rem 1.6rem;
-        background: rgba(57, 63, 84, 0.8);
-        &:after {
-            content: "";
-            position: absolute;
-            left: 0px;
-            right: 0px;
-            bottom: 0px;
-            z-index: 999;
-            height: 2px;
-            border-bottom-left-radius: 2px;
-            border-bottom-right-radius: 2px;
-            background-position: 0% 0%;
-            background: linear-gradient(
-                to right,
-                #b294ff,
-                #57e6e6,
-                #feffb8,
-                #57e6e6,
-                #b294ff,
-                #57e6e6
-            );
-            background-size: 500% auto;
-            animation: gradient 3s linear infinite;
-        }
-    }
 
-    input {
-        flex-grow: 1;
-        color: BFD2FF;
-        font-size: 1.8rem;
-        line-height: 2.4rem;
-        vertical-align: middle;
-        &::-webkit-input-placeholder {
-            color: #7881a1;
-        }
-    }
-</style>
+
+

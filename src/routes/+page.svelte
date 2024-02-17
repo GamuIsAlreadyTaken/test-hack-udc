12470a3cf953829f03c437aa8ce31e85d0e8c7f7
Alabasted <ikercapi08@gmail.com>
2024-02-17 01:44:36 +0100

    Mas arreglos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,33 @@
 <script lang="ts">
-	import Date from './../lib/inputs/components/date.svelte';
-    import type { DateFormFieldSchema } from '$lib/inputs/processors/date.processor';
+	import Number from './../lib/inputs/components/number.svelte';
+    import type { NumberFormFieldSchema } from '$lib/inputs/processors/number.processor';
 	
-	let dataP: DateFormFieldSchema ={
+	let dataP: NumberFormFieldSchema ={
 		field_description: "",
 
 		field_id: 3,
 
-		field_type: "date",
+		field_type: "number",
 
 		field_order: 4,
 
 		field_name: "date",
 
-		field_readonly:true,
+		field_readonly:false,
 		
-		field_default_value:null
+		field_default_value:2,
+
+
+		field_validations:{
+			max_value:5,
+
+
+			min_value:1
+		}
 	}
+	
 	</script>
 
 <h1>Reto de disashop</h1>
 
-<Date data = {dataP}></Date>
+<Number data = {dataP}> </Number>

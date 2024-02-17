4bff6f7eea6626bf4554e108f65cc5150751c02e
Gamu <GamuDiez@gmail.com>
2024-02-17 04:57:18 +0100

    Lo siento iker

--- src/lib/form-builder.svelte
+++ src/lib/form-builder.svelte
@@ -2,6 +2,7 @@
     import type {
         FormSchema,
         GenericFormFieldSchema,
+        GroupSchema,
     } from "./types/api-schema";
 
     // import data from "./targets.json";
@@ -14,21 +15,65 @@
 
     const url =
         "https://0fcd2366-7de6-464f-b389-b9a5533ed9af.mock.pstmn.io/api/v1/formTypes";
-    let fields: GenericFormFieldSchema[] = [];
+
+    let groupedFields: ReturnType<typeof groupElements> = {};
+    let groups: GroupSchema[] = [];
     onMount(async () => {
         const res = await fetch(url, {
             method: "GET",
             headers: { form_type_id: form_id + "" },
         });
         const formSchema: FormSchema = await res.json();
-        // form.set(formSchema);
-        fields = formSchema.form_fields
+
+        groupedFields = groupElements(formSchema.form_fields);
+        groups = formSchema.form_groups.toSorted(
+            (g1, g2) => g1.group_order - g2.group_order,
+        );
     });
+    function groupElements(
+        fields: GenericFormFieldSchema[],
+    ): Record<string, GenericFormFieldSchema[]> {
+        let result: ReturnType<typeof groupElements> = { __ungrouped: [] };
+        for (const field of fields) {
+            if (field.field_group == undefined) {
+                result["__ungrouped"].push(field);
+                continue;
+            }
+            const group = field.field_group;
+            if (result[group] == undefined) {
+                result[group] = [];
+            }
+            result[group].push(field);
+        }
+        for (const arr of Object.values(result)) {
+            arr.sort((e1, e2) => e1.field_order - e2.field_order);
+        }
+        return result;
+    }
 </script>
 
-{#each fields as data}
-    {@const type = getFieldType(data)}
-    <svelte:component this={processors[type]} {data}></svelte:component>
+{#each groups as group (group.group_id)}
+    <fieldset>
+        <legend>{group.group_name}</legend>
+        {#each groupedFields[group.group_id] as field (field.field_order)}
+            {@const type = getFieldType(field)}
+            <svelte:component this={processors[type]} data={field} />
+        {:else}
+            <p>Loading fields...</p>
+        {/each}
+    </fieldset>
 {:else}
-<p>Loading...</p>
+    <p>Loading groups...</p>
 {/each}
+
+{#each groupedFields["__ungrouped"] ?? [] as freeField (freeField.field_order)}
+    {@const type = getFieldType(freeField)}
+    <svelte:component this={processors[type]} data={freeField} />
+{/each}
+
+<style>
+    fieldset {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
